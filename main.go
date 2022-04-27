a7e66bb007ea0c5d328f93ea12213ce7c7032f78
Wang-zhetao <wzt15857662906@163.com>
2022-04-27 13:32:48 +0800

    不知道为啥文件不存在

--- main.go
+++ main.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
-	"time"
+	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+// go:embedded_file frontend/dist/index.html
+var FS embed.FS
 
 func main() {
 	go func() {
 		gin.SetMode(gin.ReleaseMode)
 		r := gin.Default()
-		r.GET("/", func(c *gin.Context) {
-			c.String(http.StatusOK, "<h1>Hello World</h1>")
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
 		r.Run(":8080")
 	}()
-	time.Sleep(time.Second * 3)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
 	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-	cnd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
-	cnd.Start()
-	select {}
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
+	cmd.Start()
+
+	select {
+	case <-c:
+		cmd.Process.Kill()
+	}
 }
